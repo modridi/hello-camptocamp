5fe0db470521bd8ab2b04e90ecdf288900a049ef
Mohamed Amine Dridi <78367821+modridi@users.noreply.github.com>
2023-10-09 14:35:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 8080
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Hello World! My name is hello-camptocamp and my owner is group:default/guests');
+  res.end('Hello Camptocamp!');
 }
 const server = http.createServer(requestListener);
 
